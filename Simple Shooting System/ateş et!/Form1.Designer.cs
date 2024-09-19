03e445231f41ba4882b554cb5d3b5e42cf889b42
emirhanapaydin <emirhan.apaydin2003@gmail.com>
2024-09-19 15:43:12 +0300

    Add files via upload

---
+++ Simple Shooting System/ateş et!/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ateş_et_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.kutuy = new System.Windows.Forms.Timer(this.components);
+            this.kutuney = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(93, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(131, 181);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 21);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(804, 221);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(108, 81);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(1028, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 520);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, -2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1042, 14);
+            this.label3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1042, 15);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(800, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(801, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 17);
+            this.label6.TabIndex = 13;
+            // 
+            // kutuy
+            // 
+            this.kutuy.Interval = 10;
+            this.kutuy.Tick += new System.EventHandler(this.kutuy_Tick);
+            // 
+            // kutuney
+            // 
+            this.kutuney.Interval = 10;
+            this.kutuney.Tick += new System.EventHandler(this.kutuney_Tick);
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(635, -6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(419, 38);
+            this.label7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(635, 415);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(419, 103);
+            this.label8.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1049, 518);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer kutuy;
+        private System.Windows.Forms.Timer kutuney;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Simple Shooting System/ateş et!/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ateş_et_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = 0;
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            pictureBox2.Visible = false;
+            timer4.Enabled = true;
+            kutuy.Enabled = true;
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                timer1.Enabled = true;
+                timer2.Enabled = false;
+
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                timer2.Enabled = true;
+                timer1.Enabled = false;
+
+
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                
+                pictureBox2.Visible = true;
+                timer5.Enabled = true;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 2;
+            if (pictureBox1.Top <= label3.Bottom)
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 2;
+            if (pictureBox1.Bottom >= label4.Top)
+            {
+                timer2.Enabled = false;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            pictureBox2.Left += 15;
+            timer4.Enabled = false;
+            if (pictureBox2.Left >= label1.Left)
+            {
+                timer4.Enabled = true;
+                timer5.Enabled = false;
+                pictureBox2.Visible = false;
+
+            }
+
+            int coryp = pictureBox2.Location.Y;
+            int corxp = pictureBox2.Location.X;
+            int cory = label5.Location.Y;
+            int cory2 = label6.Location.Y;
+            int corx = label5.Location.X;
+            int corx2 = label6.Location.X;
+            if (coryp >= cory && coryp <= cory2 && pictureBox2.Left >= pictureBox3.Left)
+            {
+                int x = pictureBox1.Location.X;
+                int y = pictureBox1.Location.Y;
+                a++;
+                label2.Text = a.ToString();
+                timer4.Enabled = true;
+                timer5.Enabled = false;
+                pictureBox2.Location = new Point(x + 10, y - 3);
+                pictureBox2.Visible = false;
+               
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (a == 10)
+            {
+              
+                timer1.Enabled = false;
+                timer3.Enabled = false;
+                timer2.Enabled = false;
+                timer4.Enabled = false;
+                timer5.Enabled = false;
+                kutuy.Enabled = false;
+                kutuney.Enabled = false;
+              
+                MessageBox.Show("Kazandın!");
+                this.Close();
+                
+                
+            }
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            int X = pictureBox2.Location.X;
+            int Y = pictureBox2.Location.Y;
+
+            pictureBox2.Location = new Point(x + 10, y - 3);
+        }
+
+        private void kutuy_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Top += 1;
+            if (pictureBox3.Bottom >= label8.Top)
+            {
+                kutuy.Enabled = false;
+                kutuney.Enabled = true;
+            }
+            int x = pictureBox3.Location.X;
+            int y = pictureBox3.Location.Y;
+            label5.Location = new Point(x , y);
+            label6.Location = new Point(x, y+50);
+           
+        }
+
+        private void kutuney_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Top -= 1;
+            if (pictureBox3.Top <= label7.Bottom)
+            {
+                kutuy.Enabled = true;
+                kutuney.Enabled = false;
+               
+            }
+            int x = pictureBox3.Location.X;
+            int y = pictureBox3.Location.Y;
+            label5.Location = new Point(x, y);
+            label6.Location = new Point(x, y + 50);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
